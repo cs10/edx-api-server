77abc3e446e4dcac0f09c1fbd48391dfb0be8eea
Michael Ball <cycomachead@gmail.com>
2015-07-09 00:04:59 -0700

    Set CORS headers for all requests

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +22,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Add CORS headers for all the requests
+app.use(cors());
+
+app.options('*', cors()); // include before other routes
 
 app.use('/', routes);
 app.use('/users', users);
